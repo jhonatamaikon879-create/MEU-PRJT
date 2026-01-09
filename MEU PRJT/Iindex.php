e5e039a16700970d3860d8fced723424d09d81cd
jhonatamaikon879-create <jhonatamaikon879@gmail.com>
2026-01-09 09:58:58 -0300

    primeiro commit : formulario php

---
+++ MEU PRJT/Iindex.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Nome</title>
+</head>
+<body>
+    <form action="processa.php" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome"><br>
+        <label for="mensagem">mensagem:</label>
+        <input type= "text" id="mensagem" name="mensagem"><br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
